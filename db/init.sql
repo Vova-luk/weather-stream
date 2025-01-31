e9c5a0bba91a3497df910519768130726c4a562d
Vova-luk <volf.lukonin@bk.ru>
2025-01-31 14:16:22 +0300

    update(weather-stream): updating table initialization

--- db/init.sql
+++ db/init.sql
@@ -3,3 +3,13 @@ CREATE TABLE locations (
     name VARCHAR(50) NOT NULL,
     coordinates VARCHAR(50) NOT NULL 
 );
+
+CREATE TABLE weather (
+    location_id INT PRIMARY KEY,
+    temperature DECIMAL(5, 2) NOT NULL, 
+    humidity INT NOT NULL,
+    wind_speed DECIMAL(5, 2) NOT NULL,
+    pressure INT NOT NULL,
+    condition VARCHAR(50) NOT NULL,
+    updated_at TIMESTAMP DEFAULT NOW()
+);
