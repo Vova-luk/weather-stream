4f7d32c530d5694464b9a67a70549ae7b57da17b
Vova-luk <volf.lukonin@bk.ru>
2025-02-03 22:48:12 +0300

    feat(weather-service): add config.go

---
+++ services/weather-service/internal/config/config.go
@@ -0,0 +1,75 @@
+package config
+
+import (
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	Server      Server
+	DB          Database
+	Kafka       Kafka
+	ExternalApi ExternalApi
+}
+
+type Server struct {
+	Port string
+}
+
+type Database struct {
+	Host     string
+	Port     string
+	User     string
+	Password string
+	DBName   string
+	SSLMode  string
+}
+
+type Kafka struct {
+	Brokers       []string
+	GroupId       string
+	LocationTopic string
+}
+
+type ExternalApi struct {
+	BaseUrl string
+	ApiKey  string
+}
+
+func LoadConfig() *Config {
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath("internal/config")
+
+	viper.ReadInConfig()
+
+	viper.SetDefault("POSTGRES_USER", "weather_user")
+	viper.SetDefault("POSTGRES_PASSWORD", "weather_pass")
+	viper.SetDefault("POSTGRES_DB", "weather_db")
+
+	viper.AutomaticEnv()
+
+	cfg := &Config{
+		Server: Server{
+			Port: viper.GetString("server.port"),
+		},
+		DB: Database{
+			Host:     viper.GetString("database.host"),
+			Port:     viper.GetString("database.port"),
+			User:     viper.GetString("POSTGRES_USER"),
+			Password: viper.GetString("POSTGRES_PASSWORD"),
+			DBName:   viper.GetString("POSTGRES_DB"),
+			SSLMode:  viper.GetString("database.sslmode"),
+		},
+		Kafka: Kafka{
+			Brokers:       viper.GetStringSlice("kafka.brokers"),
+			GroupId:       viper.GetString("kafka.group_id"),
+			LocationTopic: viper.GetString("kafka.location_topic"),
+		},
+		ExternalApi: ExternalApi{
+			BaseUrl: viper.GetString("external_api.base_url"),
+			ApiKey:  viper.GetString("API_KEY"),
+		},
+	}
+
+	return cfg
+}
