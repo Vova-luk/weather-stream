214c409477e452de07e9ebd0e7260e615724b0bd
Vova-luk <volf.lukonin@bk.ru>
2025-02-03 20:57:51 +0300

    update(weather-service): added conversion function to proto and changed data types

--- services/weather-service/internal/models/models.go
+++ services/weather-service/internal/models/models.go
@@ -1,13 +1,30 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	weatherPb "github.com/Vova-luk/weather-stream/services/weather-service/proto"
+)
 
 type WeatherData struct {
-	LocationId  int       `db:"location_id"`
+	LocationId  int32     `db:"location_id"`
 	Temperature float64   `db:"temperature"`
-	Humidity    int       `db:"humidity"`
+	Humidity    int32     `db:"humidity"`
 	WindSpeed   float64   `db:"wind_speed"`
-	Pressure    int       `db:"pressure"`
-	Condition   string    `db:"condition"`
+	Pressure    float64   `db:"pressure"`
+	Precip      float64   `db:"precip"`
+	Cloud       int32     `db:"cloud"`
 	UpdateAt    time.Time `db:"updated_at"`
+}
+
+func (w *WeatherData) ToProto() *weatherPb.WeatherData {
+	return &weatherPb.WeatherData{
+		LocationId:  w.LocationId,
+		Temperature: w.Temperature,
+		Humidity:    w.Humidity,
+		WindSpeed:   w.WindSpeed,
+		Pressure:    w.Pressure,
+		Precip:      w.Precip,
+		Cloud:       w.Cloud,
+		UpdateAt:    w.UpdateAt.GoString()}
 }
