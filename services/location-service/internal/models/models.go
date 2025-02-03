7438d8b8e4f25f43fb012f5b7a79fd97f6a3a69b
Vova-luk <volf.lukonin@bk.ru>
2025-02-03 20:38:54 +0300

    update(location-service): added function for converting to proto format

--- services/location-service/internal/models/models.go
+++ services/location-service/internal/models/models.go
@@ -1,7 +1,23 @@
 package models
 
+import (
+	locationPb "github.com/Vova-luk/weather-stream/services/location-service/proto/location"
+)
+
 type Location struct {
 	ID          int    `db:"id" json:"id"`
 	Name        string `db:"name" json:"name"`
 	Coordinates string `db:"coordinates" json:"coordinates"`
+}
+
+func ToProto(locations []*Location) []*locationPb.Location {
+	locationsProto := make([]*locationPb.Location, len(locations))
+	for idx, location := range locations {
+		locationsProto[idx] = &locationPb.Location{
+			LocationId:  int32(location.ID),
+			Name:        location.Name,
+			Coordinates: location.Coordinates,
+		}
+	}
+	return locationsProto
 }
