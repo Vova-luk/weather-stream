f333737b12b5aa1d9446e8733b8a1b92752f02c5
Vova-luk <volf.lukonin@bk.ru>
2025-02-03 20:40:16 +0300

    feat(location-service): added file for working with kafka

---
+++ services/location-service/pkg/kafka/kafka.go
@@ -0,0 +1,50 @@
+package kafka
+
+import (
+	"github.com/IBM/sarama"
+	"github.com/sirupsen/logrus"
+)
+
+func NewProducer(brokers []string, topik string, log *logrus.Logger) (sarama.SyncProducer, error) {
+	if err := ensureTopicExists(brokers, topik, log); err != nil {
+		return nil, err
+	}
+	config := sarama.NewConfig()
+	config.Producer.Return.Successes = true
+
+	producer, err := sarama.NewSyncProducer(brokers, config)
+	if err != nil {
+		return nil, err
+	}
+
+	return producer, nil
+}
+
+func ensureTopicExists(brokers []string, topic string, log *logrus.Logger) error {
+	config := sarama.NewConfig()
+
+	admin, err := sarama.NewClusterAdmin(brokers, config)
+	if err != nil {
+		return err
+	}
+	defer admin.Close()
+
+	topics, err := admin.ListTopics()
+	if err != nil {
+		return err
+	}
+
+	if _, ok := topics[topic]; !ok {
+		err := admin.CreateTopic(topic, &sarama.TopicDetail{
+			NumPartitions:     3,
+			ReplicationFactor: 1,
+		}, false)
+		if err != nil {
+			return err
+		}
+		log.Printf("Created topic: %s", topic)
+	} else {
+		log.Printf("Topic %s already exists", topic)
+	}
+	return nil
+}
