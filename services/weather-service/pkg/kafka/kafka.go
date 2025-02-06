4345d2efe02b1c6b6c3e2a143614cf3f4994e102
Vova-luk <volf.lukonin@bk.ru>
2025-02-06 09:24:54 +0300

    feat(weather-service): added creation of producer and topic

--- services/weather-service/pkg/kafka/kafka.go
+++ services/weather-service/pkg/kafka/kafka.go
@@ -2,9 +2,13 @@ package kafka
 
 import (
 	"github.com/IBM/sarama"
+	"github.com/sirupsen/logrus"
 )
 
-func NewProduces(brokers []string) (sarama.SyncProducer, error) {
+func NewProduces(brokers []string, topic string, log *logrus.Logger) (sarama.SyncProducer, error) {
+	if err := ensureTopicExists(brokers, topic, log); err != nil {
+		return nil, err
+	}
 	config := sarama.NewConfig()
 	config.Producer.Return.Successes = true
 
@@ -26,4 +30,34 @@ func NewConsumer(brokers []string, groupId string) (sarama.ConsumerGroup, error)
 	}
 
 	return consumerGroup, nil
+}
+
+func ensureTopicExists(brokers []string, topic string, log *logrus.Logger) error {
+	config := sarama.NewConfig()
+
+	admin, err := sarama.NewClusterAdmin(brokers, config)
+	if err != nil {
+		return err
+	}
+	defer admin.Close()
+
+	topiks, err := admin.ListTopics()
+	if err != nil {
+		return nil
+	}
+
+	if _, ok := topiks[topic]; !ok {
+		err := admin.CreateTopic(topic, &sarama.TopicDetail{
+			NumPartitions:     3,
+			ReplicationFactor: 1,
+		}, false)
+		if err != nil {
+			return err
+		}
+		log.Printf("Created topic: %s", topic)
+	} else {
+		log.Printf("Topic %s already exists", topic)
+	}
+	return nil
+
 }
