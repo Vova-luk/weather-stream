c8b668ca0dd640b336a6015a6db60cc10bbe50cc
Vova-luk <volf.lukonin@bk.ru>
2025-02-06 09:31:30 +0300

    feat(weather-service): added sending messages to kafka, added regular weather updates

--- services/weather-service/internal/service/weather_service.go
+++ services/weather-service/internal/service/weather_service.go
@@ -13,19 +13,24 @@ import (
 	"github.com/Vova-luk/weather-stream/services/weather-service/internal/models"
 	"github.com/Vova-luk/weather-stream/services/weather-service/internal/repository"
 	"github.com/sirupsen/logrus"
+
+	locationPb "github.com/Vova-luk/weather-stream/services/location-service/proto/location"
 )
 
 type WeatherService struct {
 	weatherRepository *repository.WeatherRepository
+	kafkaProducer     sarama.SyncProducer
 	kafkaConsumer     sarama.ConsumerGroup
+	locationClient    locationPb.LocationServiceClient
 	log               *logrus.Logger
 	cfg               *config.Config
 }
 
-func NewWeatherService(weatherRepo *repository.WeatherRepository, kafkaConsumer sarama.ConsumerGroup, log *logrus.Logger, cfg *config.Config) *WeatherService {
+func NewWeatherService(weatherRepo *repository.WeatherRepository, kafkaProducer sarama.SyncProducer, kafkaConsumer sarama.ConsumerGroup, locationClient locationPb.LocationServiceClient, log *logrus.Logger, cfg *config.Config) *WeatherService {
 	return &WeatherService{
 		weatherRepository: weatherRepo,
 		kafkaConsumer:     kafkaConsumer,
+		locationClient:    locationClient,
 		log:               log,
 		cfg:               cfg,
 	}
@@ -34,8 +39,10 @@ func NewWeatherService(weatherRepo *repository.WeatherRepository, kafkaConsumer 
 func (w *WeatherService) GetWeatherById(locationId int32) (*models.WeatherData, error) {
 	location, err := w.weatherRepository.GetWeatherById(locationId)
 	if err != nil {
+		w.log.Warnf("Ошибка получения данных по айди %v", err)
 		return nil, err
 	}
+	w.log.Infof("получены данные из бд")
 	return location, nil
 }
 
@@ -46,7 +53,6 @@ func (w *WeatherService) StartKafkaConsumer(topik string) {
 		err := w.kafkaConsumer.Consume(context.Background(), []string{topik}, w)
 		if err != nil {
 			w.log.Printf("error reading message %v", err)
-			time.Sleep(5 * time.Second)
 		}
 	}
 }
@@ -64,7 +70,7 @@ func (w *WeatherService) Setup(sarama.ConsumerGroupSession) error {
 func (w *WeatherService) ConsumeClaim(session sarama.ConsumerGroupSession, claim sarama.ConsumerGroupClaim) error {
 	for msg := range claim.Messages() {
 		var location struct {
-			LocationId  int
+			LocationId  int32
 			Name        string
 			Coordinates string
 		}
@@ -77,6 +83,7 @@ func (w *WeatherService) ConsumeClaim(session sarama.ConsumerGroupSession, claim
 		}
 
 		w.log.Printf("Message received from Kafka: %+v", err)
+		w.log.Println("LocationId: %d", location.LocationId)
 
 		if err := w.AddCurrentWeather(location.LocationId, location.Name, location.Coordinates); err != nil {
 			w.log.Errorf("error when updating weather %v", err)
@@ -90,34 +97,104 @@ func (w *WeatherService) ConsumeClaim(session sarama.ConsumerGroupSession, claim
 	return nil
 }
 
-func (w *WeatherService) AddCurrentWeather(locationId int, name, coordinates string) error {
+func (w *WeatherService) SendingRequest(locationId int32, coordinates string) (*models.WeatherData, error) {
 
 	url := fmt.Sprintf("%s?key=%s&q=%s", w.cfg.ExternalApi.BaseUrl, w.cfg.ExternalApi.ApiKey, coordinates)
 
 	resp, err := http.Get(url)
 	if err != nil {
 		w.log.Warnf("error sending request %v", err)
-		return err
+		return nil, err
 	}
 	defer resp.Body.Close()
 
 	buf, err := io.ReadAll(resp.Body)
 	if err != nil {
 		w.log.Warnf("error reading response body %v", err)
-		return err
+		return nil, err
 	}
 
 	var WeatherInformation models.WeatherInformation
 	if err := json.Unmarshal(buf, &WeatherInformation); err != nil {
 		w.log.Infof("Received JSON: %s", string(buf))
 		w.log.Warnf("error decoding JSON %v", err)
-		return err
+		return nil, err
 	}
 
 	weatherData := WeatherInformation.ToWeatherData(locationId)
+	return weatherData, nil
+}
+
+func (w *WeatherService) AddCurrentWeather(locationId int32, name, coordinates string) error {
+	weatherData, err := w.SendingRequest(locationId, coordinates)
+	if err != nil {
+		w.log.Warnf("something is wrong with sending the request %v", err)
+		return err
+	}
 
 	if err := w.weatherRepository.AddForcefullyWeather(weatherData); err != nil {
 		w.log.Warnf("error when updating weather information from Id %d: %v", locationId, err)
+		return err
+	}
+
+	return nil
+}
+
+func (w *WeatherService) UpdateCurrentWeather(locationId int32, name, coordinates string) (*models.WeatherData, error) {
+	weatherData, err := w.SendingRequest(locationId, coordinates)
+	if err != nil {
+		w.log.Warnf("something is wrong with sending the request %v", err)
+		return nil, err
+	}
+
+	if err := w.weatherRepository.UpdateCurrentWeather(weatherData); err != nil {
+		w.log.Warnf("error in updating current weather data in the database %v", err)
+		return nil, err
+	}
+
+	return weatherData, nil
+}
+
+func (w *WeatherService) UpdatesRegularWeather(topic string) {
+	for {
+		request := &locationPb.Empty{}
+		locations, err := w.locationClient.GetLocations(context.Background(), request)
+		if err != nil {
+			w.log.Warnf("error getting locations from location-service %v", err)
+		}
+
+		for _, location := range locations.GetLocations() {
+			weatherData, err := w.UpdateCurrentWeather(location.GetLocationId(), location.GetName(), location.GetCoordinates())
+			if err != nil {
+				w.log.Warnf("error updating current weather %v", err)
+				continue
+			}
+
+			if err := w.SendMessageToKafka(weatherData, topic); err != nil {
+				w.log.Warnf("failed to send message %v", err)
+				continue
+			}
+
+		}
+		time.Sleep(5 * time.Minute)
+	}
+}
+
+func (w *WeatherService) SendMessageToKafka(weatherData *models.WeatherData, topic string) error {
+	message, err := json.Marshal(weatherData)
+	if err != nil {
+		w.log.Warnf("JSON serialization error %v", err)
+		return err
+	}
+
+	msg := &sarama.ProducerMessage{
+		Topic: topic,
+		Value: sarama.StringEncoder(message),
+	}
+
+	_, _, err = w.kafkaProducer.SendMessage(msg)
+	if err != nil {
+		w.log.Warnf("error sending message to kafka %v", err)
 		return err
 	}
 
