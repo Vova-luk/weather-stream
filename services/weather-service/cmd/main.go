58b38ebf76a3c0eb0013451889c7fb8bd4787c64
Vova-luk <volf.lukonin@bk.ru>
2025-02-06 09:24:07 +0300

    feat(weather-service): added work with kafka producer

--- services/weather-service/cmd/main.go
+++ services/weather-service/cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net"
 
+	locationPb "github.com/Vova-luk/weather-stream/services/location-service/proto/location"
 	"github.com/Vova-luk/weather-stream/services/weather-service/internal/config"
 	"github.com/Vova-luk/weather-stream/services/weather-service/internal/handler"
 	"github.com/Vova-luk/weather-stream/services/weather-service/internal/logger"
@@ -24,27 +25,48 @@ func main() {
 		log.Fatalf("Bad Connect to Postgre %s", err.Error())
 	}
 
-	consumerGroup, err := kafka.NewConsumer(cfg.Kafka.Brokers, cfg.Kafka.GroupId)
+	kafkaBrokers := cfg.Kafka.Brokers
+
+	consumerGroup, err := kafka.NewConsumer(kafkaBrokers, cfg.Kafka.GroupId)
 	if err != nil {
 		log.Fatalf("error creating consumer %v", err)
 	}
 
+	producer, err := kafka.NewProduces(kafkaBrokers, cfg.Kafka.WeatherTopic, log)
+	if err != nil {
+		log.Fatalf("error creating producer %v", err)
+	}
+
+	grpcAddrLocation := "location-service" + cfg.Server.LocationServicePort
+
+	locationConn, err := grpc.Dial(grpcAddrLocation, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Error connecting to location-service %s", err.Error())
+	}
+	log.Printf("Connected to location-service on the port: %d", 50051)
+
+	locationClient := locationPb.NewLocationServiceClient(locationConn)
+
 	weatherRepo := repository.NewWeatherRepository(db)
-	weatherService := service.NewWeatherService(weatherRepo, consumerGroup, log, cfg)
+	weatherService := service.NewWeatherService(weatherRepo, producer, consumerGroup, locationClient, log, cfg)
 	weatherHandler := handler.NewWeatherHandler(weatherService)
 
-	weatherService.StartKafkaConsumer(cfg.Kafka.LocationTopic)
+	go func() {
+		weatherService.StartKafkaConsumer(cfg.Kafka.LocationTopic)
+	}()
 
 	grpcServer := grpc.NewServer()
 	weatherPb.RegisterWeatherServiceServer(grpcServer, weatherHandler)
 
 	grpcAddr := ":" + cfg.Server.Port
 
+	log.Infof("🚀 Starting gRPC server on port %s", grpcAddr)
 	listen, err := net.Listen("tcp", grpcAddr)
 	if err != nil {
-		log.Fatalf("Failed to listen on port %v", err)
+		log.Fatalf("Failed to listen on port:%s, %v", grpcAddr, err)
 	}
 
+	log.Info("gRPC server started")
 	if err := grpcServer.Serve(listen); err != nil {
 		log.Fatalf("Failed to serve gRPC: %s", err)
 	}
